dac2cd706c08432fde82e8986d1d72fc8dbc78ef
Marc David <7226788+wkwhwh@users.noreply.github.com>
2025-02-08 15:33:33 +0000

    layout adjustments

--- keyboards/ferris/keymaps/wkwhwh/keymap.c
+++ keyboards/ferris/keymaps/wkwhwh/keymap.c
@@ -4,14 +4,17 @@
 #define END G(KC_RGHT)
 #define FWD G(KC_RBRC)
 #define BACK G(KC_LBRC)
+#define SW_WIN G(KC_GRV)
+#define INIT C(S(A(G(KC_I))))
+#define EMOJI (C(G(KC_SPACE)))
 #define AE_TILE A(KC_DOT)
 #define AE_ACRD A(KC_COMM)
 #define AE_LNCH S(A(KC_QUOT))
 #define AE_SRVC S(A(KC_SCLN))
+#define AE_BACK A(KC_LBRC)
+#define AE_FWRD A(KC_RBRC)
 #define LA_SYM MO(SYM)
 #define LA_NAV MO(NAV)
-#define LOCK G(C(KC_Q))
-#define INIT C(S(A(G(KC_I))))
 
 enum layers {
     DEF,
@@ -30,13 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [SYM] = LAYOUT(
         KC_ESC,        KC_LBRC,       KC_LCBR,       KC_LPRN,       KC_TILD,           KC_CIRC,       KC_RPRN,       KC_RCBR,       KC_RBRC,       KC_GRV,
         KC_MINS,       KC_ASTR,       KC_EQL,        KC_UNDS,       KC_DLR,            KC_HASH,       OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LCTL),
-        KC_PLUS,       KC_PIPE,       KC_AT,         KC_SLSH,       KC_PERC,           XXXXXXX,       KC_BSLS,       KC_AMPR,       KC_QUES,       KC_EXLM,
+        KC_PLUS,       KC_PIPE,       KC_AT,         KC_SLSH,       KC_PERC,           EMOJI,         KC_BSLS,       KC_AMPR,       KC_QUES,       KC_EXLM,
                                                      _______,       _______,           _______,       _______
     ),
     [NAV] = LAYOUT(
-        KC_TAB,        AE_TILE,       AE_ACRD,       KC_VOLD,       KC_VOLU,           QK_BOOT,       HOME,          KC_UP,         END,           KC_DEL,
-        OSM(MOD_LCTL), OSM(MOD_LSFT), OSM(MOD_LALT), OSM(MOD_LGUI), KC_MPLY,           INIT,          KC_LEFT,       KC_DOWN,       KC_RGHT,       KC_BSPC,
-        AE_LNCH,       AE_SRVC,       BACK,          FWD,           XXXXXXX,           LOCK,          KC_PGDN,       KC_PGUP,       KC_CAPS,       KC_ENT,
+        KC_TAB,        SW_WIN,        AE_BACK,       AE_FWRD,       AE_SRVC,           KC_MPRV,       HOME,          KC_UP,         END,           KC_DEL,
+        OSM(MOD_LCTL), OSM(MOD_LSFT), OSM(MOD_LALT), OSM(MOD_LGUI), AE_LNCH,           KC_MNXT,       KC_LEFT,       KC_DOWN,       KC_RGHT,       KC_BSPC,
+        AE_TILE,       AE_ACRD,       BACK,          FWD,           INIT,              KC_MPLY,       KC_PGDN,       KC_PGUP,       KC_CAPS,       KC_ENT,
                                                      _______,       _______,           _______,       _______
     ),
     [NUM] = LAYOUT(
